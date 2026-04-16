80e8f8cabb475f13a06c63b47086b93dee5494e2
nixsuperdai@gmail.com <xiaoxindeng>
2026-04-16 12:17:13 +0800

    print observation space for debug

--- robobase/workspace.py
+++ robobase/workspace.py
@@ -53,6 +53,9 @@ def _create_default_replay_buffer(
             shape=(),
             dtype=np.uint8,
         )
+        print("=== eval_env.observation_space ===")
+        for k, v in observation_space.items():
+            print(f"{k}: shape={v.shape}, dtype={v.dtype}")
         observation_space["rgb_external"] = spaces.Box(low=0, high=255, shape=(3, 84, 84), dtype=np.uint8)
     # Create replay_class with buffer-specific hyperparameters
     replay_class = UniformReplayBuffer
